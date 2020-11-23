6fe5bfca21d9566d919e73773d2410d86e8c9c3c
ericaphillips <ericaphill@gmail.com>
2020-11-23 11:07:54 -0600

    finished chapter 1

---
+++ src/Customer.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Customer.css"
+
+export const Customer = () => (
+    <section className="customer">
+        <h3 className="customer__name">P Sherman</h3>
+        <div className="customer__address">42 Wallaby Way</div>
+    </section>
+)
---
+++ src/Employee.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css"
+
+export const Employee = () => (
+    <section className="employee">
+        <h3 className="employee__name">Dory</h3>
+        <div className="employee__location">Kennels of Sydney</div>
+    </section>
+)
