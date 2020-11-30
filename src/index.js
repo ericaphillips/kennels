9700df8785813e9facd207f9feeb2f1e64d88cf4
ericaphillips <ericaphill@gmail.com>
2020-11-30 11:28:51 -0600

    finished chapter 6

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import { Animal } from './components/animal/Animal';
-// import { Employee } from './Employee';
-// import { Customer } from './Customer';
-// import { Location } from  './Location';
-import { Kennel } from './components/Kennel'
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Kennel } from "./components/Kennel.js"
+import "./index.css"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Kennel />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <Router>
+            <Kennel />
+        </Router>
+    </React.StrictMode>,
+    document.getElementById("root")
+)
